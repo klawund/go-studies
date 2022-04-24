0c0ba5d4ea403a0cab11137640f0d4d38c752dc7
klawund <klabundehiago@gmail.com>
2022-04-24 17:25:47 -0300

    metodos

--- curso-aprenda-golang-do-zero/15. metodos/methods.go
+++ curso-aprenda-golang-do-zero/15. metodos/methods.go
@@ -12,10 +12,12 @@ type user struct {
 // passando o tipo antes do nome, a função será atrelada a esse tipo, e pode usar os seus campos
 // o tipo é inferido via parâmetro, então há uma variável "u" de tipo user
 // u fica disponível dentro do método e user fica inferido como o tipo atrelado ao método
+// se o tipo for passado sem ponteiro, o objeto do struct é passado via cópia e o objeto original fica inalterado
 func (u user) save() {
 	fmt.Printf("salvando os dados de %s no banco\n", u.name)
 }
 
+// como user é passado via ponteiro, esse método consegue alterar os dados reais do objeto que o acessou
 func (u *user) alterName(newName string) {
 	u.name = newName
 }
